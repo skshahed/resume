6f4add7ac4a2d58b27ad86641b8d40e70f3f588b
Sk. Abu Shahed Alam <sha_hed007@yahoo.com>
2022-02-18 23:43:42 +0600

    Update settings.py

--- resume/settings.py
+++ resume/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #SECRET_KEY = os.getenv('SECRET_KEY')
 SECRET_KEY = 'mga-@c92207k4$qaaddfgsd5sdfs5a3*e6u82l=#l7eswlr+swmgal+@'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'sheikhshahed.herokuapp.com', 'www.sheikhshahed.herokuapp.com']
+ALLOWED_HOSTS = ['sheikhshahed.herokuapp.com', 'www.sheikhshahed.herokuapp.com']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
